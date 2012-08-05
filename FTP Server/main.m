26457fd28a93b619d3af68be3187af878301bbac
Keith Duncan <keith@realmacsoftware.com>
2012-08-05 18:45:17 +0100

    Initial Commit

---
+++ FTP Server/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  FTP Server
+//
+//  Created by Keith Duncan on 05/08/2012.
+//  Copyright (c) 2012 Realmac Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
