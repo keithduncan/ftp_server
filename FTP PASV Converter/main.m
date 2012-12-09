18492ef8e953907bc5436859de80bd984bfa9c50
Keith Duncan <keith@realmacsoftware.com>
2012-12-09 22:17:32 +0000

    Add PASV parser for working with servers using the x-man-page://telnet command

---
+++ FTP PASV Converter/main.m
@@ -0,0 +1,53 @@
+//
+//  main.m
+//  FTP PASV Converter
+//
+//  Created by Keith Duncan on 09/12/2012.
+//  Copyright (c) 2012 Keith Duncan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+static void Usage(int argc, char const **argv)
+{
+	NSLog(@"Usage: %s h1,h2,h3,h4,p1,p2", argv[0]);
+}
+
+int main(int argc, char const **argv)
+{
+	@autoreleasepool {
+		if (argc <= 1) {
+			Usage(argc, argv);
+			return -1;
+		}
+		
+		char const *pasvStringBytes = argv[1];
+		NSString *pasvString = [NSString stringWithCString:pasvStringBytes encoding:NSUTF8StringEncoding];
+		
+		NSArray *components = [pasvString componentsSeparatedByString:@","];
+		if ([components count] != 6) {
+			Usage(argc, argv);
+			return -1;
+		}
+		
+		int h1 = 0, h2 = 0, h3 = 0, h4 = 0;
+		
+		int hIdx = 0;
+		h1 = [components[hIdx++] intValue];
+		h2 = [components[hIdx++] intValue];
+		h3 = [components[hIdx++] intValue];
+		h4 = [components[hIdx++] intValue];
+		
+		int p1 = 0, p2 = 0;
+		
+		int pIdx = hIdx;
+		p1 = [components[pIdx++] intValue];
+		p2 = [components[pIdx++] intValue];
+		
+		int port = ((p1 * 256) + p2);
+		
+		NSLog(@"%d.%d.%d.%d %d", h1, h2, h3, h4, port);
+	}
+    return 0;
+}
+
